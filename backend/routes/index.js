fe05d52a5ff733e2aae7f96ea8f1bbcdc4a85cc0
Mustafa <mustafaomousa@icloud.com>
2021-01-21 17:37:23 -0600

    created backend api routes following RESTful convention

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
+const apiRouter = require('./api');
+
+router.use('/api', apiRouter);
+
 //creating a test route:
 router.get('/hello/world', function (req, res) {
     res.cookie('XSRF-TOKEN', req.csrfToken());
