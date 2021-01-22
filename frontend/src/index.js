fac509235bed5b002edd12771ca5fc1952ed5fc3
Mustafa <mustafaomousa@icloud.com>
2021-01-22 13:24:53 -0600

    completed custom fetch and restoring xsrf-token cookies

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,13 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider as ReduxProvider } from 'react-redux';
+
 import './index.css';
 import App from './App';
 import configureStore from './store';
+import { restoreCSRF, fetch } from './store/csrf';
 
 const store = configureStore();
 
 if (process.env.NODE_ENV !== "production") {
+  restoreCSRF();
+
+  window.csrfFetch = fetch;
   window.store = store;
 }
 
---
+++ frontend/src/store/csrf.js
@@ -0,0 +1,28 @@
+import Cookies from 'js-cookie';
+
+export async function fetch(url, options = {}) {
+    options.method = options.method || 'GET';
+    options.headers = options.headers || {};
+
+    if (options.method.toUpperCase() !== 'GET') {
+        options.headers['Content-Type'] =
+            options.headers['Content-Type'] || 'application/json';
+        options.headers['XSRF-Token'] = Cookies.get('XSRF-TOKEN');
+    };
+
+    const res = await window.fetch(url, options);
+
+    const contentType = res.headers.get('content-type');
+    if (contentType && contentType.includes('application/json')) {
+        const data = await res.json();
+        res.data = data;
+    }
+
+    if (res.status >= 400) throw res;
+
+    return res;
+};
+
+export function restoreCSRF() {
+    return fetch('/api/csrf/restore');
+};
