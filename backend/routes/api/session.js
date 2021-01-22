31a45a20d929c31169135552491c20cd2ba14243
Mustafa <mustafaomousa@icloud.com>
2021-01-22 11:45:28 -0600

    completed login route in my /api/session file

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -1,5 +1,28 @@
 const express = require('express');
+const asyncHandler = require('express-async-handler');
+
+const { setTokenCookie, restoreUser } = require('../../utils/auth');
+const { User } = require('../../db/models');
 
 const router = express.Router();
+
+//login route
+router.post('/', asyncHandler(async (req, res, next) => {
+    const { credential, password } = req.body;
+
+    const user = await User.login({ credential, password });
+
+    if (!user) {
+        const err = new Error('Login failed');
+        err.status = 401;
+        err.title = 'Login failed';
+        err.errors = ['The provided credentials were invalid.'];
+        return next(err);
+    }
+
+    await setTokenCookie(res, user);
+
+    return res.json({ user });
+}));
 
 module.exports = router;
