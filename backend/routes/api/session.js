d7e9f0518b833bf1cf37bb435b1884f80de07a50
Mustafa <mustafaomousa@icloud.com>
2021-01-22 12:10:04 -0600

    completed restore user route in /api/session

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -31,4 +31,15 @@ router.delete('/', (_req, res) => {
     return res.json({ message: 'success' });
 });
 
+router.get('/', restoreUser, (req, res) => {
+    const { user } = req;
+    if (user) {
+        return res.json({
+            user: user.toSafeObject()
+        });
+    } else {
+        return res.json({});
+    }
+});
+
 module.exports = router;
