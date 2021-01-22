e0bd46c3f9991f7f89f43e28a094acf059725381
Mustafa <mustafaomousa@icloud.com>
2021-01-21 18:59:25 -0600

    i created some user auth routes

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -48,46 +48,47 @@ module.exports = (sequelize, DataTypes) => {
   User.associate = function (models) {
     // associations can be defined here
   };
+
+  User.prototype.toSafeObject = function () {
+    const { id, username, email } = this;
+    return { id, username, email };
+  };
+
+  User.prototype.validatePassword = function (password) {
+    return bcrypt.compareSync(password, this.hashedPassword.toString());
+  };
+
+  User.getCurrentUserById = async function (id) {
+    return await User.scope('currentUser').findByPk(id);
+  };
+
+
+
+  User.login = async function ({ credential, password }) {
+    const { Op } = require('sequelize');
+    const user = await User.scope('loginUser').findOne({
+      where: {
+        [Op.or]: {
+          username: credential,
+          email: credential,
+        }
+      }
+    });
+
+    if (user && user.validatePassword(password)) {
+      return await User.scope('currentUser').findByPk(user.id);
+    };
+  };
+
+  User.signup = async function ({ username, email, password }) {
+    const hashedPassword = bcrypt.hashSync(password);
+    const user = await User.create({
+      username,
+      email,
+      hashedPassword,
+    });
+    return await User.scope('currentUser').findByPk(user.id);
+  };
   return User;
 };
 
-User.prototype.toSafeObject = function () {
-  const { id, username, email } = this;
-  return { id, username, email };
-};
-
-User.prototype.validatePassword = function (password) {
-  return bcrypt.compareSync(password, this.hashedPassword.toString());
-};
-
-User.getCurrentUserById = async function (id) {
-  return await User.scope('currentUser').findByPk(id);
-};
-
-
-
-User.login = async function ({ credential, password }) {
-  const { Op } = require('sequelize');
-  const user = await User.scope('loginUser').findOne({
-    where: {
-      [Op.or]: {
-        username: credential,
-        email: credential,
-      }
-    }
-  });
-
-  if (user && user.validatePassword(password)) {
-    return await User.scope('currentUser').findByPk(user.id);
-  };
-};
-
-User.signup = async function ({ username, email, password }) {
-  const hashedPassword = bcrypt.hashSync(password);
-  const user = await User.create({
-    username,
-    email,
-    hashedPassword,
-  });
-  return await User.scope('currentUser').findByPk(user.id);
-};
--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,7 +1,19 @@
 const router = require('express').Router();
+const asyncHandler = require('express-async-handler');
+
+const { setTokenCookie, restoreUser, requireAuth } = require('../../utils/auth.js');
+const { User } = require('../../db/models');
+
+const sessionRouter = require('./session.js');
+const usersRouter = require('./users.js');
+
+router.use('/session', sessionRouter);
+router.use('/users', usersRouter);
+
 
 router.post('/test', function (req, res) {
     res.json({ requestBody: req.body });
 });
+
 
 module.exports = router;
