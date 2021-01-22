8f80727ddd003c392958fea1a46016cfc887ccb4
Mustafa <mustafaomousa@icloud.com>
2021-01-22 13:07:27 -0600

    initial react/redux setup

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -6,9 +6,9 @@ const apiRouter = require('./api');
 router.use('/api', apiRouter);
 
 //creating a test route:
-router.get('/hello/world', function (req, res) {
-    res.cookie('XSRF-TOKEN', req.csrfToken());
-    res.send('Hello World!');
-});
+// router.get('/hello/world', function (req, res) {
+//     res.cookie('XSRF-TOKEN', req.csrfToken());
+//     res.send('Hello World!');
+// });
 
 module.exports = router;
