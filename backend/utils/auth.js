bf0af193ad86208ca586b3d7b1d8abc81111c5ac
Mustafa <mustafaomousa@icloud.com>
2021-01-21 18:48:27 -0600

    created user auth middlewares

---
+++ backend/utils/auth.js
@@ -0,0 +1,65 @@
+const jwt = require('jsonwebtoken');
+const { jwtConfig } = require('../config');
+const { User } = require('../db/models');
+
+const { secret, expiresIn } = jwtConfig;
+
+//to be used forsetting the jwt cookie after a user is logged in or signed up
+const setTokenCookie = (res, user) => {
+    const token = jwt.sign(
+        { data: user.toSafeObject() },
+        secret,
+        { expiresIn: parseInt(expiresIn) },
+    );
+
+    const isProduction = process.env.NODE_ENV === 'production';
+
+    res.cookie('token', token, {
+        maxAge: expiresIn * 1000,
+        httpOnly: true,
+        secure: isProduction,
+        sameSite: isProduction && 'Lax',
+    });
+
+    return token;
+};
+
+
+//to be used to identify the current session user
+const restoreUser = (req, res, next) => {
+    const { token } = req.cookies;
+
+    return jwt.verify(token, secret, null, async (err, jwtPayload) => {
+        if (err) {
+            return next();
+        }
+
+        try {
+            const { id } = jwtPayload.data;
+            req.user = await User.scope('currentUser').findByPk(id);
+        } catch (e) {
+            res.clearCookie('token');
+            return next();
+        }
+
+        if (!req.user) res.clearCookie('token');
+
+        return next();
+    })
+};
+
+//to be used to require a session user to be authenticated before accessing a route
+const requireAuth = [
+    restoreUser,
+    function (req, _res, next) {
+        if (req.user) return next();
+
+        const err = new Error('Unauthorized');
+        err.title = 'Unauthorized';
+        err.errors = ['Unauthorized'];
+        err.status = 401;
+        return next(err);
+    }
+];
+
+module.exports = { setTokenCookie, restoreUser, requireAuth };
