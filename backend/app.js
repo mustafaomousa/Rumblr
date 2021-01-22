168cc765cf04f82a58c2265d16d7fdd218ca9495
Mustafa <mustafaomousa@icloud.com>
2021-01-21 18:02:51 -0600

    setup error handling

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const csurf = require('csurf');
 const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
+const { ValidationError } = require('sequelize');
 
 const { environment } = require('./config');
 const routes = require('./routes');
@@ -39,5 +40,35 @@ app.use(csurf({
 }));
 
 app.use(routes);
+
+//Connecting the resource not found error handler:
+app.use((_req, _res, next) => {
+    const err = new Error("The requested resource couldn't be found.");
+    err.title = "Resource Not Found";
+    err.errors = ["The requested resource couldn't be found."];
+    err.status = 404;
+    next(err);
+});
+
+//Process sequelize errors
+app.use((err, _req, _res, next) => {
+    if (err instanceof ValidationError) {
+        err.errors = err.errors.map(e => e.message);
+        err.title = 'Sequelize validation error';
+    }
+    next(err);
+});
+
+//Error Formatter Error-Handler
+app.use((err, _req, res, _next) => {
+    res.status(err.status || 500);
+    console.error(err);
+    res.json({
+        title: err.title || 'Server Error',
+        message: err.message,
+        errors: err.errors,
+        stack: isProduction ? null : err.stack,
+    });
+});
 
 module.exports = app;
