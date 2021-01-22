9737181d67845078c20a6400cd3ac3c3ed7a54d2
Mustafa <mustafaomousa@icloud.com>
2021-01-22 12:55:43 -0600

    created my login express-validators

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -1,13 +1,27 @@
 const express = require('express');
 const asyncHandler = require('express-async-handler');
+const { check } = require('express-validator');
 
 const { setTokenCookie, restoreUser } = require('../../utils/auth');
+const { handleValidationErrors } = require('../../utils/validation');
 const { User } = require('../../db/models');
 
 const router = express.Router();
 
+//validate my login req
+const validateLogin = [
+    check('credential')
+        .exists({ checkFalsy: true })
+        .notEmpty()
+        .withMessage('Please provide a valid email or username.'),
+    check('password')
+        .exists({ checkFalsy: true })
+        .withMessage('Please provide a password.'),
+    handleValidationErrors,
+];
+
 //login route
-router.post('/', asyncHandler(async (req, res, next) => {
+router.post('/', validateLogin, asyncHandler(async (req, res, next) => {
     const { credential, password } = req.body;
 
     const user = await User.login({ credential, password });
