4bfaa7d49f8c5a08854b3202dc1929c41a66424f
Mustafa <mustafaomousa@icloud.com>
2021-01-22 18:19:06 -0600

    updated my user model file user.signup function to include header bio and profpic

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -101,11 +101,14 @@ module.exports = (sequelize, DataTypes) => {
     };
   };
 
-  User.signup = async function ({ username, email, password }) {
+  User.signup = async function ({ username, email, header, bio, profilePicture, password }) {
     const hashedPassword = bcrypt.hashSync(password);
     const user = await User.create({
       username,
       email,
+      header,
+      bio,
+      profilePicture,
       hashedPassword,
     });
     return await User.scope('currentUser').findByPk(user.id);
