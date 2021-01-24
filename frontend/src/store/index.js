a783e37583177887edb207fdc4d32368dc9b0843
Mustafa <mustafaomousa@icloud.com>
2021-01-24 11:24:10 -0600

    created a reducer and thunk action for retrieving all posts, going to try and dispatch on load next

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -2,9 +2,11 @@ import { createStore, combineReducers, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 
 import sessionReducer from './session';
+import postReducer from './post';
 
 const rootReducer = combineReducers({
   session: sessionReducer,
+  posts: postReducer,
 });
 
 let enhancer;
---
+++ frontend/src/store/post.js
@@ -0,0 +1,34 @@
+const GET_POSTS = 'post/GET_POSTS';
+
+const getAllPosts = allPosts => {
+    return {
+        type: GET_POSTS,
+        payload: allPosts,
+    };
+};
+
+export const getPosts = () => async dispatch => {
+    const response = await fetch('/api/posts');
+
+    if (response.ok) {
+        dispatch(getAllPosts(response.data.allPosts));
+        return response;
+    }
+};
+
+const initialState = { posts: null };
+
+const postReducer = (state = initialState, action) => {
+    let newState;
+
+    switch (action.type) {
+        case GET_POSTS:
+            newState = Object.assign({}, state);
+            newState.posts = action.payload;
+            return newState;
+        default:
+            return state;
+    };
+};
+
+export default postReducer;
