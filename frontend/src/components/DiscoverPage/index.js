8a6c748bd99540bf2c700a5b6527494dc6eca8d4
mustafaomousa <mustafaomousa@icloud.com>
2021-10-06 17:43:55 -0500

    done with initial discover page redesign

--- frontend/src/components/DiscoverPage/index.js
+++ frontend/src/components/DiscoverPage/index.js
@@ -17,183 +17,18 @@ import FavoriteIcon from "@mui/icons-material/Favorite";
 import SmartButtonIcon from "@mui/icons-material/SmartButton";
 import "./index.css";
 import CreatePost from "../CreatePost";
+import PostCard from "../PostCard";
 
 const FeedPage = () => {
   const sessionUser = useSelector((state) => state.session.user);
+  const posts = useSelector((state) => state.posts.allPosts);
 
   if (!sessionUser) return <Redirect to="/" />;
 
   return (
     <Stack className="DiscoverPage" spacing={5}>
       <CreatePost user={sessionUser} />
-      <Card sx={{ width: "500px" }}>
-        <CardHeader
-          avatar={<Avatar></Avatar>}
-          action={
-            <IconButton>
-              <MoreVertIcon />
-            </IconButton>
-          }
-          title="username here"
-        />
-        <CardMedia
-          component="img"
-          // height="00px"
-          image="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/rs-e-tron-gt-tango-red-10-1628022210.jpg?crop=0.845xw:0.842xh;0.0459xw,0.0356xh&resize=640:*"
-          alt="image"
-        />
-        <CardContent>
-          <Typography>post body here</Typography>
-        </CardContent>
-        <CardActions>
-          <IconButton>
-            <FavoriteIcon />
-          </IconButton>
-          <IconButton>
-            <SmartButtonIcon />
-          </IconButton>
-        </CardActions>
-      </Card>
-      <Card sx={{ width: "500px" }}>
-        <CardHeader
-          avatar={<Avatar></Avatar>}
-          action={
-            <IconButton>
-              <MoreVertIcon />
-            </IconButton>
-          }
-          title="username here"
-        />
-        <CardMedia
-          component="img"
-          // height="00px"
-          image="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/rs-e-tron-gt-tango-red-10-1628022210.jpg?crop=0.845xw:0.842xh;0.0459xw,0.0356xh&resize=640:*"
-          alt="image"
-        />
-        <CardContent>
-          <Typography>post body here</Typography>
-        </CardContent>
-        <CardActions>
-          <IconButton>
-            <FavoriteIcon />
-          </IconButton>
-          <IconButton>
-            <SmartButtonIcon />
-          </IconButton>
-        </CardActions>
-      </Card>
-      <Card sx={{ width: "500px" }}>
-        <CardHeader
-          avatar={<Avatar></Avatar>}
-          action={
-            <IconButton>
-              <MoreVertIcon />
-            </IconButton>
-          }
-          title="username here"
-        />
-        <CardMedia
-          component="img"
-          // height="00px"
-          image="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/rs-e-tron-gt-tango-red-10-1628022210.jpg?crop=0.845xw:0.842xh;0.0459xw,0.0356xh&resize=640:*"
-          alt="image"
-        />
-        <CardContent>
-          <Typography>post body here</Typography>
-        </CardContent>
-        <CardActions>
-          <IconButton>
-            <FavoriteIcon />
-          </IconButton>
-          <IconButton>
-            <SmartButtonIcon />
-          </IconButton>
-        </CardActions>
-      </Card>
-      <Card sx={{ width: "500px" }}>
-        <CardHeader
-          avatar={<Avatar></Avatar>}
-          action={
-            <IconButton>
-              <MoreVertIcon />
-            </IconButton>
-          }
-          title="username here"
-        />
-        <CardMedia
-          component="img"
-          // height="00px"
-          image="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/rs-e-tron-gt-tango-red-10-1628022210.jpg?crop=0.845xw:0.842xh;0.0459xw,0.0356xh&resize=640:*"
-          alt="image"
-        />
-        <CardContent>
-          <Typography>post body here</Typography>
-        </CardContent>
-        <CardActions>
-          <IconButton>
-            <FavoriteIcon />
-          </IconButton>
-          <IconButton>
-            <SmartButtonIcon />
-          </IconButton>
-        </CardActions>
-      </Card>
-      <Card sx={{ width: "500px" }}>
-        <CardHeader
-          avatar={<Avatar></Avatar>}
-          action={
-            <IconButton>
-              <MoreVertIcon />
-            </IconButton>
-          }
-          title="username here"
-        />
-        <CardMedia
-          component="img"
-          // height="00px"
-          image="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/rs-e-tron-gt-tango-red-10-1628022210.jpg?crop=0.845xw:0.842xh;0.0459xw,0.0356xh&resize=640:*"
-          alt="image"
-        />
-        <CardContent>
-          <Typography>post body here</Typography>
-        </CardContent>
-        <CardActions>
-          <IconButton>
-            <FavoriteIcon />
-          </IconButton>
-          <IconButton>
-            <SmartButtonIcon />
-          </IconButton>
-        </CardActions>
-      </Card>
-      <Card sx={{ width: "500px" }}>
-        <CardHeader
-          avatar={<Avatar></Avatar>}
-          action={
-            <IconButton>
-              <MoreVertIcon />
-            </IconButton>
-          }
-          title="username here"
-        />
-        <CardMedia
-          component="img"
-          // height="00px"
-          image="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/rs-e-tron-gt-tango-red-10-1628022210.jpg?crop=0.845xw:0.842xh;0.0459xw,0.0356xh&resize=640:*"
-          alt="image"
-        />
-        <CardContent>
-          <Typography>post body here</Typography>
-        </CardContent>
-        <CardActions>
-          <IconButton>
-            <FavoriteIcon />
-          </IconButton>
-          <IconButton>
-            <SmartButtonIcon />
-          </IconButton>
-        </CardActions>
-      </Card>
+      {posts && posts.map((post) => <PostCard post={post} />)}
     </Stack>
   );
 };
--- frontend/src/components/PostCard/index.js
+++ frontend/src/components/PostCard/index.js
@@ -1,118 +1,79 @@
-import { useEffect, useState } from "react";
-import { useSelector, useDispatch } from "react-redux";
-import { Card, CardHeader } from "@mui/material";
-
-import { createNewLike, deleteLike } from "../../store/like";
 import {
-  createRerumble,
-  deletePost,
-  removeRerumble,
-  updatePost,
-} from "../../store/post";
+  Avatar,
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  CardHeader,
+  CardMedia,
+  IconButton,
+  Popover,
+  Typography,
+} from "@mui/material";
+import DeleteIcon from "@mui/icons-material/Delete";
+import EditIcon from "@mui/icons-material/Edit";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import SmartButtonIcon from "@mui/icons-material/SmartButton";
+import { deletePost } from "../../store/post";
+import { useDispatch, useSelector } from "react-redux";
+import { useState } from "react";
+import { Box } from "@mui/system";
 
-const PostCard = ({ post, rerumbles }) => {
+const PostCard = ({ post }) => {
   const dispatch = useDispatch();
-  const [liked, setLiked] = useState(false);
-  const [pictureIsOpen, setPictureIsOpen] = useState(false);
-  const [toolsOpen, setToolsOpen] = useState(false);
+  const [openPopover, setOpenPopover] = useState(false);
   const sessionUser = useSelector((state) => state.session.user);
-  const postLikes = useSelector((state) =>
-    state.likes.likes.filter((like) => like.postId === post.id)
-  );
-  const userLikes = useSelector((state) =>
-    state.likes.likes.filter(
-      (like) => like.userId === sessionUser.id && like.postId === post.id
-    )
-  );
-  const rerumble = rerumbles.filter(
-    (rerumble) =>
-      rerumble.userId === sessionUser.id && rerumble.postId === post.id
-  )[0];
+  const togglePopover = () => setOpenPopover(!openPopover);
 
-  let like = null;
-  if (userLikes.shift !== undefined) {
-    like = userLikes.shift();
-  }
-
-  const likePost = (e) => {
+  const deleteUserPost = (e) => {
     e.preventDefault();
-    setLiked(true);
-    const userId = sessionUser.id;
-    const postId = post.id;
-    const payload = { userId, postId };
-
-    dispatch(createNewLike(payload));
+    dispatch(deletePost({ postId: post.id }));
   };
-
-  let listOfTags = [];
-  if (post.Tags) {
-    for (let i = 0; i < post.Tags.length; i++) {
-      listOfTags.push(post.Tags[i].name);
-    }
-  }
-
-  const openSelectedPicture = (e) => {
-    e.preventDefault();
-    setPictureIsOpen(true);
-  };
-
-  const removeLike = (e) => {
-    e.preventDefault();
-
-    const userId = sessionUser.id;
-    const postId = post.id;
-    const payload = { userId, postId };
-
-    dispatch(deleteLike(payload));
-    setLiked(false);
-  };
-
-  const addRerumble = (e) => {
-    e.preventDefault();
-
-    const userId = sessionUser.id;
-    const postId = post.id;
-    const payload = { userId, postId };
-
-    dispatch(createRerumble(payload));
-  };
-
-  const deleteRerumble = (e) => {
-    e.preventDefault();
-
-    const userId = sessionUser.id;
-    const postId = post.id;
-    const payload = { userId, postId };
-
-    dispatch(removeRerumble(payload));
-  };
-
-  const [updateOpen, setUpdateOpen] = useState(false);
-  const [title, setTitle] = useState(post.title);
-  const [body, setBody] = useState(post.body);
-
-  const onUpdate = (e) => {
-    e.preventDefault();
-
-    const tags = body.match(/#[A-Za-z0-9]*/g);
-
-    const payload = {
-      tags,
-      postId: post.id,
-      title,
-      body,
-    };
-    dispatch(updatePost(payload));
-    setUpdateOpen(false);
-  };
-
-  useEffect(() => {
-    if (like) setLiked(true);
-  }, [like]);
 
   return (
-    <Card style={{ width: "900px" }}>
-      <CardHeader title="username here" />
+    <Card sx={{ width: "500px" }}>
+      <CardHeader
+        avatar={<Avatar src={post.User.profilePicture} />}
+        action={
+          sessionUser.id === post.User.id && (
+            <>
+              <IconButton aria-describedby="test">
+                <DeleteIcon onClick={deleteUserPost} />
+              </IconButton>
+              <IconButton>
+                <EditIcon />
+              </IconButton>
+            </>
+          )
+        }
+        title={post.User.username}
+      />
+      <CardMedia
+        component="img"
+        // height="00px"
+        image={post.content}
+        alt="image"
+      />
+      <CardContent>
+        <Typography>{post.body}</Typography>
+      </CardContent>
+      <CardActions sx={{ display: "flex", justifyContent: "space-between" }}>
+        <div>
+          <IconButton>
+            <FavoriteIcon />
+          </IconButton>
+          <IconButton>
+            <SmartButtonIcon />
+          </IconButton>
+        </div>
+        <div style={{ overflow: "scroll" }}>
+          {post.Tags.map((tag) => (
+            <Button size="small">
+              <Typography>{tag.name}</Typography>
+            </Button>
+          ))}
+        </div>
+      </CardActions>
     </Card>
   );
 };
