135ee3526f0f09f85cb7900392bb159d86f822b2
mustafaomousa <mustafaomousa@icloud.com>
2021-12-16 19:34:08 -0600

    updated newest members

--- frontend/src/components/Discover/NewestMembers.js
+++ frontend/src/components/Discover/NewestMembers.js
@@ -4,50 +4,65 @@ import {
   Card,
   CardContent,
   CardHeader,
-  CircularProgress,
   Divider,
+  Stack,
   Typography,
 } from "@mui/material";
+import { makeStyles } from "@mui/styles";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
 import { getNewestUsers } from "../../store/discover";
 
+const useStyles = makeStyles(() => ({
+  root: {
+    width: "400px",
+  },
+  cardHeader: {
+    backgroundColor: "#333A56",
+  },
+}));
+
 const NewestMembers = () => {
   const dispatch = useDispatch();
+  const classes = useStyles();
 
   const newestUsers = useSelector((state) => state.discover.newestUsers);
 
   useEffect(() => dispatch(getNewestUsers()), []);
 
-  if (newestUsers)
-    return (
-      <div>
-        <Card
-          className="discover-page-side-newest-members"
-          variant="outlined"
-          sx={{ borderRadius: "0.1em" }}
-        >
-          <CardHeader
-            align="center"
-            title={<Typography>Our newest members</Typography>}
-          />
-          <CardContent>
-            {newestUsers &&
-              newestUsers.map((newestUser) => {
-                return (
-                  <Button key={newestUser.id} id="newest-user-button">
-                    <Avatar src={newestUser.profilePicture} />
-                    <Typography>{newestUser.username}</Typography>
-                    <Divider />
-                  </Button>
-                );
-              })}
-          </CardContent>
-        </Card>
-      </div>
-    );
-  else return <CircularProgress />;
+  return (
+    <Card className={classes.root}>
+      <CardHeader
+        className={classes.cardHeader}
+        title={
+          <Typography fontWeight="bold" color="#ffffff">
+            Our newest members
+          </Typography>
+        }
+      />
+      <Divider />
+      <CardContent>
+        <Stack spacing={2}>
+          {newestUsers &&
+            newestUsers.map((newestUser) => (
+              <Button
+                variant="outlined"
+                key={newestUser.id}
+                sx={{ justifyContent: "start" }}
+              >
+                <Avatar
+                  src={newestUser.profilePicture}
+                  sx={{ marginRight: 2 }}
+                />
+                <Typography>{newestUser.username}</Typography>
+                <Divider />
+              </Button>
+            ))}
+        </Stack>
+      </CardContent>
+    </Card>
+  );
 };
 
 export default NewestMembers;
