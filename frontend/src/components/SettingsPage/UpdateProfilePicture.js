76ced55ec0267bec844814af73fb0ef1b3d51c9c
mustafaomousa <mustafaomousa@icloud.com>
2021-12-22 11:31:48 -0600

    completed initial draft account settings page

---
+++ frontend/src/components/SettingsPage/UpdateProfilePicture.js
@@ -0,0 +1,70 @@
+import { Avatar, Button, Input, Stack, Typography } from "@mui/material";
+import { useDispatch } from "react-redux";
+import S3FileUpload from "react-s3/lib/ReactS3";
+import { updateProfilePicture } from "../../store/session";
+
+const UpdateProfilePicture = ({ sessionUser, updateSessionUser }) => {
+  const dispatch = useDispatch();
+
+  const config = {
+    bucketName: "rumblr-app",
+    dirName: sessionUser.username.profile_picture,
+    region: "us-east-2",
+    accessKeyId: process.env.REACT_APP_ACCESS_ID,
+    secretAccessKey: process.env.REACT_APP_SECRET_ACCESS_ID,
+  };
+
+  const updateProfilePic = async (e) => {
+    e.preventDefault();
+
+    await S3FileUpload.uploadFile(e.target.files[0], config).then((data) => {
+      dispatch(updateProfilePicture(sessionUser.id, data.location));
+    });
+    //   .then(() => restoreUser());
+  };
+  return (
+    <form>
+      <Typography color="primary" variant="h6" gutterBottom={2}>
+        Profile picture
+      </Typography>
+      <Avatar
+        variant="square"
+        sx={{
+          width: "100%",
+          height: "auto",
+          boxShadow:
+            "rgba(50, 50, 105, 0.15) 0px 2px 5px 0px, rgba(0, 0, 0, 0.05) 0px 1px 1px 0px",
+        }}
+        src={sessionUser.profilePicture}
+      />
+      <Stack
+        spacing={1}
+        direction="row"
+        justifyContent={"flex-end"}
+        padding="10px 0px"
+      >
+        <Input
+          type="file"
+          id="profile-image"
+          sx={{ display: "none" }}
+          onChange={updateProfilePic}
+        />
+        <label htmlFor="profile-image">
+          <Button size="small" variant="contained" component="span">
+            Select Image
+          </Button>
+        </label>
+        <Button
+          color="warning"
+          size="small"
+          variant="contained"
+          onClick={() => dispatch(updateProfilePicture(sessionUser.id, null))}
+        >
+          Delete
+        </Button>
+      </Stack>
+    </form>
+  );
+};
+
+export default UpdateProfilePicture;
--- frontend/src/components/SettingsPage/index.js
+++ frontend/src/components/SettingsPage/index.js
@@ -19,6 +19,7 @@ import useGlobalStyles from "../useGlobalStyles";
 import UpdateBio from "./UpdateBio";
 import UpdatePersonalInformation from "./UpdatePersonalInformation";
 import UpdatePassword from "./UpdatePassword";
+import UpdateProfilePicture from "./UpdateProfilePicture";
 
 const SettingsPage = () => {
   const dispatch = useDispatch();
@@ -31,23 +32,6 @@ const SettingsPage = () => {
     region: "us-east-2",
     accessKeyId: process.env.REACT_APP_ACCESS_ID,
     secretAccessKey: process.env.REACT_APP_SECRET_ACCESS_ID,
-  };
-
-  const updateProfilePic = async (e) => {
-    e.preventDefault();
-
-    await S3FileUpload.uploadFile(e.target.files[0], config)
-      .then((data) => {
-        dispatch(updateProfilePicture(sessionUser.id, data.location));
-      })
-      .then(() => restoreUser());
-  };
-
-  const testupdateUsername = async () => {
-    await fetch(`/api/users/${sessionUser.username}`, {
-      method: "PUT",
-      body: JSON.stringify({ username: "demouser" }),
-    });
   };
 
   return (
@@ -66,33 +50,10 @@ const SettingsPage = () => {
           <Box sx={{ border: "5px solid #333A56", padding: "10px 25px" }}>
             <Grid container padding={2} spacing={3} gridAutoFlow={"row"}>
               <Grid item xs={4}>
-                <Typography color="primary" variant="h6" gutterBottom={2}>
-                  Profile picture
-                </Typography>
-                <Avatar
-                  variant="square"
-                  sx={{
-                    width: "100%",
-                    height: "auto",
-                    background: "#ffffff",
-                    boxShadow:
-                      "rgba(50, 50, 105, 0.15) 0px 2px 5px 0px, rgba(0, 0, 0, 0.05) 0px 1px 1px 0px",
-                  }}
-                  src={sessionUser.profilePicture}
+                <UpdateProfilePicture
+                  sessionUser={sessionUser}
+                  updateSessionUser={updateSessionUser}
                 />
-                <Stack
-                  spacing={1}
-                  direction="row"
-                  justifyContent={"flex-end"}
-                  padding="10px 0px"
-                >
-                  <Button size="small" variant="contained">
-                    Select Image
-                  </Button>
-                  <Button color="warning" size="small" variant="contained">
-                    Delete
-                  </Button>
-                </Stack>
               </Grid>
               <Grid item xs={8}>
                 <UpdateBio
