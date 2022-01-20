013a6fdb6e7d9892661ae274edeaee70d9d7baed
mustafaomousa <mustafaomousa@icloud.com>
2022-01-19 18:52:16 -0600

    changed csurf

--- backend/app.js
+++ backend/app.js
@@ -1,20 +1,20 @@
-const express = require('express');
-const morgan = require('morgan');
-const cors = require('cors');
-const csurf = require('csurf');
-const helmet = require('helmet');
-const cookieParser = require('cookie-parser');
-const { ValidationError } = require('sequelize');
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const csurf = require("csurf");
+const helmet = require("helmet");
+const cookieParser = require("cookie-parser");
+const { ValidationError } = require("sequelize");
 
-const { environment } = require('./config');
-const routes = require('./routes');
-const isProduction = environment === 'production';
+const { environment } = require("./config");
+const routes = require("./routes");
+const isProduction = environment === "production";
 
 //initializing express application:
 const app = express();
 
 //connecting morgan middleware:
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 
 //adding cookie parser middleware for parsing cookies
 //adding express.json middleware for parsing json bodies:
@@ -24,51 +24,55 @@ app.use(express.json());
 //adding several security middlewares:
 //cors, helment, csurf
 if (!isProduction) {
-    app.use(cors());
-};
+  app.use(cors());
+}
 
-app.use(helmet({
-    contentSecurityPolicy: false
-}));
+app.use(
+  helmet({
+    contentSecurityPolicy: false,
+  })
+);
 
-app.use(csurf({
+app.use(
+  csurf({
     cookie: {
-        secure: isProduction,
-        sameSite: isProduction && "Lax",
-        httpOnly: true
-    }
-}));
+      secure: isProduction,
+      // sameSite: isProduction && "Lax",
+      httpOnly: true,
+    },
+  })
+);
 
 app.use(routes);
 
 //Connecting the resource not found error handler:
 app.use((_req, _res, next) => {
-    const err = new Error("The requested resource couldn't be found.");
-    err.title = "Resource Not Found";
-    err.errors = ["The requested resource couldn't be found."];
-    err.status = 404;
-    next(err);
+  const err = new Error("The requested resource couldn't be found.");
+  err.title = "Resource Not Found";
+  err.errors = ["The requested resource couldn't be found."];
+  err.status = 404;
+  next(err);
 });
 
 //Process sequelize errors
 app.use((err, _req, _res, next) => {
-    if (err instanceof ValidationError) {
-        err.errors = err.errors.map(e => e.message);
-        err.title = 'Sequelize validation error';
-    }
-    next(err);
+  if (err instanceof ValidationError) {
+    err.errors = err.errors.map((e) => e.message);
+    err.title = "Sequelize validation error";
+  }
+  next(err);
 });
 
 //Error Formatter Error-Handler
 app.use((err, _req, res, _next) => {
-    res.status(err.status || 500);
-    console.error(err);
-    res.json({
-        title: err.title || 'Server Error',
-        message: err.message,
-        errors: err.errors,
-        stack: isProduction ? null : err.stack,
-    });
+  res.status(err.status || 500);
+  console.error(err);
+  res.json({
+    title: err.title || "Server Error",
+    message: err.message,
+    errors: err.errors,
+    stack: isProduction ? null : err.stack,
+  });
 });
 
 module.exports = app;
